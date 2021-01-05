eecb066c0b6e86a9447239869daafc8ed1be8dc0
chaos7ds <chaos7ds@gmail.com>
2021-01-05 20:30:28 +0900

    fix validate

--- validate.py
+++ validate.py
@@ -12,28 +12,16 @@ n = [str(i) for i in range(10)]
 for i in os.listdir(d):
     mod = 0
     for j in range(len(i)):
-        if mod == 10 and i[j] == '.':
-            mod = 11
-        if mod == 9 and i[j] == ')':
-            mod = 10
-        if mod == 8 and i[j] == '(':
-            mod = 9
-        if mod == 7 and i[j] == '_':
-            mod = 8
-        if mod == 6 and i[j] in n:
-            mod = 7
-        if mod == 5 and i[j] in n:
-            mod = 6
-        if mod == 4 and i[j] in n:
+        if mod == 4 and i[j] == ')' and i[j+1] == '.':
             mod = 5
-        if mod == 3 and i[j] in n:
+        if mod == 3 and i[j] == '_' and i[j+1] == '(':
             mod = 4
-        if mod == 2 and i[j] in n:
+        if mod == 2 and i[j] in n and i[j+1] in n and i[j+2] in n and i[j+3] in n and i[j+4] in n:
             mod = 3
         if mod == 1 and i[j] == '_':
             mod = 2
         if mod == 0 and i[j] == '_':
             mod = 1
 
-    if mod != 11:
+    if mod != 5:
         print(i)
